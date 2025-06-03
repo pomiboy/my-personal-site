77657d4bcea643947f7924bafc39e3c3b85d147a
pomiboy <embiidjoel76@gmail.com>
2025-06-03 16:09:14 +0900

    connect link with icons

--- src/components/custom/IconRow.tsx
+++ src/components/custom/IconRow.tsx
@@ -6,32 +6,40 @@ import {
   LinkedinLogoIcon,
   PhoneIcon,
 } from "@phosphor-icons/react";
+import Link from "next/link";
 import { CustomTooltip } from "./Tooltip";
+import { Links } from "../../constants/links";
 
 export default function IconRow() {
   return (
     <div className="flex gap-[25px]">
       <CustomTooltip
         trigger={
-          <div className="hover:text-[#007ebc] duration-100 cursor-pointer">
-            <LinkedinLogoIcon size={28} weight="bold" />
-          </div>
+          <Link href={Links.LINKEDIN} target="_blank">
+            <div className="hover:text-[#007ebc] duration-100 cursor-pointer">
+              <LinkedinLogoIcon size={28} weight="bold" />
+            </div>
+          </Link>
         }
         content={"LinkedIn"}
       />
       <CustomTooltip
         trigger={
-          <div className="hover:text-[#616161] duration-100 cursor-pointer">
-            <GithubLogoIcon size={28} weight="bold" />
-          </div>
+          <Link href={Links.GITHUB} target="_blank">
+            <div className="hover:text-[#616161] duration-100 cursor-pointer">
+              <GithubLogoIcon size={28} weight="bold" />
+            </div>
+          </Link>
         }
         content={"Github"}
       />
       <CustomTooltip
         trigger={
-          <div className="hover:text-[#e8514f] duration-100 cursor-pointer">
-            <InstagramLogoIcon size={28} weight="bold" />
-          </div>
+          <Link href={Links.INSTAGRAM} target="_blank">
+            <div className="hover:text-[#e8514f] duration-100 cursor-pointer">
+              <InstagramLogoIcon size={28} weight="bold" />
+            </div>
+          </Link>
         }
         content={"Instagram"}
       />
---
+++ src/constants/links.ts
@@ -0,0 +1,5 @@
+export const Links = {
+  LINKEDIN: "https://www.linkedin.com/in/seokryu-park-417941301/",
+  GITHUB: "https://github.com/pomiboy",
+  INSTAGRAM: "https://www.instagram.com/pomiryu_/",
+};
